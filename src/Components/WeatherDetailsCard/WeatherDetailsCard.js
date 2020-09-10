baec3fbd8e043ffd657357bfaa4e5a9f592aa1b3
Francisco Egea <puertorey@gmail.com>
2020-09-10 17:02:50 +0100

    Installing Sass

--- src/Components/WeatherDetailsCard/WeatherDetailsCard.js
+++ src/Components/WeatherDetailsCard/WeatherDetailsCard.js
@@ -6,30 +6,28 @@ const now = new Date();
 
 const WeatherDetailsCard = (props) => {
 	return(
-		<div>
-			<div className="card mw5 center bg-near-white br3 pa3 mv3 ba b--black-50">
-			  <div>
-			    <img 
-			    	src={`http://openweathermap.org/img/wn/${props.data.icon}@2x.png`}
-			    	className="h4 w4 dib pa1 bg-light-silver br-100 b--black-50" 
-			    	title="weather icon" 
-			    	alt='Icon not available'
-			    />
-			  </div>
-			  <div>
-			  	<div>
-						<span className='f2 mb2'>{props.city}</span>
-					</div>
-					<div>
-						<span className='f3 mb2'>{Math.round(props.data.temperature)} °C</span>
-					</div>
-					<div>
-						<span className="f5 fw4 gray mt0">{props.data.description}</span>
-					</div>
-					<br/>
-					<div className="i f6">
-						{dateFormat(now, "dddd, mmmm dS, yyyy, h:MM:ss TT")}
-					</div>
+		<div className="card mw5 center bg-near-white br3 pa3 mv3 ba b--black-50">
+		  <div>
+		    <img 
+		    	src={`http://openweathermap.org/img/wn/${props.data.icon}@2x.png`}
+		    	className="h4 w4 dib pa1 bg-light-silver br-100 b--black-50" 
+		    	title="weather icon" 
+		    	alt='Icon not available'
+		    />
+		  </div>
+		  <div>
+		  	<div>
+					<span className='f2 mb2'>{props.city}</span>
+				</div>
+				<div>
+					<span className='f3 mb2'>{Math.round(props.data.temperature)} °C</span>
+				</div>
+				<div>
+					<span className="f5 fw4 gray mt0">{props.data.description}</span>
+				</div>
+				<br/>
+				<div className="i f6">
+					{dateFormat(now, "dddd, mmmm dS, yyyy, h:MM:ss TT")}
 				</div>
 			</div>
 		</div>
